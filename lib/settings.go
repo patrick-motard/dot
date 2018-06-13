7c09ba0314c397f24b9b7490813e6c17e9de7cc6
patrick-motard <motard19@gmail.com>
2018-06-12 23:16:38 -0500

    now calls screenlayout script defined in settings
    - settings now allows paths to be relative (including ~ as prefix in path)
    - path assumed when ~ is /home/$USER

--- lib/settings.go
+++ lib/settings.go
@@ -1,10 +1,13 @@
 package lib
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 	"reflect"
 	"strings"
 )
@@ -58,11 +61,45 @@ func GetSettings() {
 	cf := strings.Join(ss, "/")
 	// fmt.Println(strings.Join(ss, "/"))
 	fmt.Println(reflect.TypeOf(cf))
-	r, err := ioutil.ReadFile(cf)
+	// r, err := ioutil.ReadFile(cf)
+	cf = fullPath(cf)
+	fmt.Println(fullPath(cf))
 
-	fmt.Println("r:", reflect.TypeOf(r))
+	// var  (
+	// 	cmdOut []byte
+	// 	err2   error
+	// )
+	// cmdName := "/bin/bash"
+	// cmdArgs := []string{cf}
+	// // cmd := exec.Command("bash", "/home/han/hello_world")
+	// // fmt.Println(err)
+	// if cmdOut, err2 = exec.Command(cmdName, cmdArgs...).Output(); err2 != nil {
+	// 	fmt.Fprintln(os.Stderr, "There was an error running the feh script: ", err2)
+	// 	os.Exit(1)
+	// }
+
+	// out := string(cmdOut)
+	// fmt.Println("OUTPUT: " + out)
+
+	// fmt.Println("r:", reflect.TypeOf(r))
 	// rs := string(r)
-	fmt.Println(ss)
-	fmt.Println(string(r))
+	fmt.Println("cf:", cf)
+	// fmt.Println(string(r))
 	// return c
+
+	cmd := exec.Command("/bin/bash", cf)
+	var out bytes.Buffer
+	cmd.Stderr = &out
+	err3 := cmd.Run()
+	if err3 != nil {
+		log.Fatal(err3)
+	}
+	fmt.Println("Output std: ", out.String())
+}
+
+func fullPath(s string) string {
+	if strings.HasPrefix(s, "~/") {
+		s = "/home/" + os.Getenv("USER") + strings.TrimPrefix(s, "~")
+	}
+	return s
 }
